90df941b7509f5332b685d74f7a86fb3d6284522
kiranreddy29 <kiranreddy2905@gmail.com>
2026-05-07 22:35:38 +0530

    department-v1 : 8 : removed local dev settings for compliance

--- FusionIIIT/Fusion/settings/development.py
+++ FusionIIIT/Fusion/settings/development.py
@@ -7,13 +7,12 @@ SECRET_KEY = '=&w9due426k@l^ju1=s1)fj1rnpf0ok8xvjwx+62_nc-f12-8('
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': {
+     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fusionlab',
-        'HOST': '127.0.0.1',
+        'HOST': os.environ.get("DB_HOST", default='localhost'),
         'USER': 'fusion_admin',
-        'PASSWORD': '2013',
-        'PORT': '5433',
+        'PASSWORD': 'hello123',
     }
 }
 
